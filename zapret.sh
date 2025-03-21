5f97d24618d68339c9eace37bc3390a5e4e17f68
rita <137996547+ritascarlet@users.noreply.github.com>
2025-03-21 14:44:38 +0300

    Update zapret.sh

--- zapret.sh
+++ zapret.sh
@@ -14,7 +14,6 @@ echo ""
 echo ""
 echo ""
 
-
 # 1. Создать скрипт для автоматического получения конфига
 cat > /etc/init.d/getdomains << EOF
 #!/bin/sh /etc/rc.common
@@ -25,41 +24,54 @@ start () {
     DOMAINS=https://raw.githubusercontent.com/AnotherProksY/allow-domains-no-youtube/main/Russia/inside-dnsmasq-nfset.lst
     count=0
     while true; do
-        if curl -m 3 github.com; then
-            curl -f $DOMAINS --output /tmp/dnsmasq.d/domains.lst
+        if curl -m 3 github.com > /dev/null 2>&1; then
+            curl -f \$DOMAINS --output /tmp/dnsmasq.d/domains.lst
             break
         else
-            echo "GitHub is not available. Check the internet availability [$count]"
-            count=$((count+1))
+            echo "GitHub is not available. Check the internet availability [\$count]"
+            count=\$((count+1))
         fi
     done
 
-    if dnsmasq --conf-file=/tmp/dnsmasq.d/domains.lst --test 2>&1 | grep -q "syntax check OK"; then
+    if dnsmasq --conf-dir=/tmp/dnsmasq.d --test 2>&1 | grep -q "syntax check OK"; then
         /etc/init.d/dnsmasq restart
     fi
 }
 EOF
 
+# Даем права на выполнение
+chmod +x /etc/init.d/getdomains
 
+# Обновляем пакеты
 opkg update
 
-
+# Скачиваем и устанавливаем пакеты
 wget -O /tmp/zapret.ipk https://raw.githubusercontent.com/ritascarlet/testforwork/main/zapret_70.20250213_aarch64_cortex-a53.ipk
+wget -O /tmp/luciapp.ipk https://raw.githubusercontent.com/ritascarlet/testforwork/main/luci-app-zapret_70.20250213_all.ipk
 
-wget -O /tmp/luciapp.ipk https://github.com/ritascarlet/testforwork/raw/refs/heads/main/luci-app-zapret_70.20250213_all.ipk
+if opkg install /tmp/zapret.ipk; then
+    echo "Zapret installed successfully."
+else
+    echo "Failed to install Zapret."
+    exit 1
+fi
 
+if opkg install /tmp/luciapp.ipk; then
+    echo "LuCI app installed successfully."
+else
+    echo "Failed to install LuCI app."
+    exit 1
+fi
 
-opkg install /tmp/zapret.ipk
-
-opkg install /tmp/luciapp.ipk
-
+# Удаляем временные файлы
 rm /tmp/zapret.ipk
-
 rm /tmp/luciapp.ipk
 
+# Запускаем getdomains
 /etc/init.d/getdomains start
 
-
+# Добавляем в автозагрузку
+/etc/init.d/getdomains enable
 
 echo ""
 echo ""
